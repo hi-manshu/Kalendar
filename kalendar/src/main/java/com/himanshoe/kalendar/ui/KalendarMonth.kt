c51ec48bb0667cd8b1990c57a8f4205f0fe3b718
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-01-18 07:45:39 +0100

    Updated the KalendarView

--- kalendar/src/main/java/com/himanshoe/kalendar/ui/KalendarMonth.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/ui/KalendarMonth.kt
@@ -1,57 +1,97 @@
 package com.himanshoe.kalendar.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import com.himanshoe.kalendar.DayOfWeek
-import kotlin.math.ceil
+import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+import java.time.Year
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+
+private const val DAYS_IN_WEEK = 7
 
 @Composable
-fun KalendarMonth(
-    currentYear: Int,
-    currentMonth: Int,
-    currentMonthNumberOfDays: Int,
-    todayYear: Int,
-    todayMonth: Int,
-    todayDayOfMonth: Int,
-    selectedYear: Int,
-    selectedMonth: Int,
-    selectedDayOfMonth: Int,
-    startOfMonthDayOfWeek: DayOfWeek,
-    onDateClick: (Int, Int, Int) -> Unit
+internal fun KalendarMonth(
+    yearMonth: YearMonth = YearMonth.now(),
+    onCurrentDayClick: (LocalDate) -> Unit,
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
-            .graphicsLayer(clip = false)
-
+            .padding(4.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
     ) {
-        val numberOfRows = ceil((currentMonthNumberOfDays + startOfMonthDayOfWeek.value) / DAYS_IN_WEEK.toDouble()).toInt()
-        var startDay = 1
-        repeat(numberOfRows) {
-            val startColumn = if (it == 0) startOfMonthDayOfWeek.value else 0
-            val endColumn = if (it != numberOfRows - 1) 6 else currentMonthNumberOfDays - startDay
-
-            KalendarWeek(
-                startColumn = startColumn,
-                endColumn = endColumn,
-                startDay = startDay,
-                currentYear = currentYear,
-                currentMonth = currentMonth,
-                todayYear = todayYear,
-                todayMonth = todayMonth,
-                todayDayOfMonth = todayDayOfMonth,
-                selectedYear = selectedYear,
-                selectedMonth = selectedMonth,
-                selectedDayOfMonth = selectedDayOfMonth,
-                onDateClick = onDateClick,
+        val monthState = remember {
+            mutableStateOf(yearMonth)
+        }
+        val selectedDay = remember {
+            mutableStateOf(LocalDate.now())
+        }
+        val formatter = remember {
+            DateTimeFormatter.ofPattern(
+                if (Year.now().value == monthState.value.year) "MMMM" else "MMMM y"
             )
-            startDay += endColumn - startColumn + 1
+        }
+        KalendarHeader(
+            text = monthState.value.format(formatter),
+            onPreviousMonthClick = {
+                monthState.value = monthState.value.minusMonths(1)
+            },
+            onNextMonthClick = {
+                monthState.value = monthState.value.plusMonths(1)
+            },
+        )
+        KalendarWeekDayNames()
+
+        val days: List<LocalDate> = getDays(monthState)
+
+        days.chunked(DAYS_IN_WEEK).forEach { weekDays ->
+            BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+                val size = (maxWidth / DAYS_IN_WEEK)
+                Row(horizontalArrangement = Arrangement.spacedBy(0.dp)) {
+                    weekDays.forEach { localDate ->
+                        val isFromCurrentMonth = YearMonth.from(localDate) == monthState.value
+                        if (isFromCurrentMonth) {
+                            val isSelected = monthState.value.month == selectedDay.value.month &&
+                                    monthState.value.year == selectedDay.value.year &&
+                                    localDate == selectedDay.value
+
+                            KalendarDay(
+                                size = size,
+                                date = localDate,
+                                isSelected = isSelected,
+                                isToday = localDate == LocalDate.now(),
+                                onDayClick = { date ->
+                                    selectedDay.value = date
+                                    onCurrentDayClick(date)
+                                }
+                            )
+                        } else {
+                            KalendarEmptyDay(modifier = Modifier.size(size))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun getDays(monthState: MutableState<YearMonth>): List<LocalDate> {
+    return mutableListOf<LocalDate>().apply {
+        val firstDay = monthState.value.atDay(1)
+        val firstSunday = if (firstDay.dayOfWeek == java.time.DayOfWeek.SUNDAY) {
+            firstDay
+        } else {
+            firstDay.minusDays(firstDay.dayOfWeek.value.toLong())
+        }
+        repeat(6) { weekIndex ->
+            (0..6).forEach { dayIndex ->
+                add(firstSunday.plusDays((7 * weekIndex + dayIndex).toLong()))
+            }
         }
     }
 }
--- kalendar/src/main/java/com/himanshoe/kalendar/ui/KalendarYear.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/ui/KalendarYear.kt
@@ -6,60 +6,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import com.himanshoe.design.theme.Grid
 import com.himanshoe.design.theme.KalendarTheme
-import com.himanshoe.kalendar.DayOfWeek
 import com.himanshoe.kalendar.config.KalendarKonfig
-import com.himanshoe.kalendar.getDayOfWeek
-import java.util.*
+import java.time.LocalDate
+import java.time.YearMonth
 
 @Composable
-private fun KalendarView(
-    currentYear: Int,
-    currentMonth: Int,
-    currentMonthNumberOfDays: Int,
-    todayYear: Int,
-    todayMonth: Int,
-    todayDayOfMonth: Int,
-    selectedYear: Int,
-    selectedMonth: Int,
-    selectedDayOfMonth: Int,
-    startOfMonthDayOfWeek: DayOfWeek,
-    onCurrentDayClick: (Int, Int, Int) -> Unit,
-    onPreviousMonthClick: () -> Unit,
-    onNextMonthClick: () -> Unit
+internal fun KalendarView(
+    yearMonth: YearMonth = YearMonth.now(),
+    onCurrentDayClick: (LocalDate) -> Unit,
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(Grid.Half)
     ) {
-        KalendarHeader(
-            currentYear = currentYear,
-            currentMonth = currentMonth,
-            onPreviousMonthClick = onPreviousMonthClick,
-            onNextMonthClick = onNextMonthClick
-        )
-        KalendarWeekDayNames()
         KalendarMonth(
-            currentYear = currentYear,
-            currentMonth = currentMonth,
-            currentMonthNumberOfDays = currentMonthNumberOfDays,
-            todayYear = todayYear,
-            todayMonth = todayMonth,
-            todayDayOfMonth = todayDayOfMonth,
-            selectedYear = selectedYear,
-            selectedMonth = selectedMonth,
-            selectedDayOfMonth = selectedDayOfMonth,
-            startOfMonthDayOfWeek = startOfMonthDayOfWeek,
-            onDateClick = onCurrentDayClick
+            yearMonth,
+            onCurrentDayClick,
         )
     }
 }
@@ -68,27 +36,8 @@ private fun KalendarView(
 fun Kalendar(
     modifier: Modifier = Modifier,
     kalendarKonfig: KalendarKonfig = KalendarKonfig(),
+    onCurrentDayClick: (LocalDate) -> Unit,
 ) {
-    val currentCalendarStartOfMonth = rememberSaveable {
-        Calendar.getInstance().apply {
-            set(Calendar.DATE, 1)
-        }
-    }
-    val currentDate = rememberSaveable { Calendar.getInstance() }
-    var currentYear by rememberSaveable { mutableStateOf(currentDate.get(Calendar.YEAR)) }
-    var currentMonth by rememberSaveable { mutableStateOf(currentDate.get(Calendar.MONTH)) }
-
-    val currentMonthNumberOfDays =
-        rememberSaveable { currentDate.getActualMaximum(Calendar.DAY_OF_MONTH) }
-
-    val todayDate = Calendar.getInstance()
-    val todayYear = todayDate.get(Calendar.YEAR)
-    val todayMonth = todayDate.get(Calendar.MONTH)
-    val todayDayOfMonth = todayDate.get(Calendar.DAY_OF_MONTH)
-
-    var selectedYear by rememberSaveable { mutableStateOf(todayYear) }
-    var selectedMonth by rememberSaveable { mutableStateOf(todayMonth) }
-    var selectedDayOfMonth by rememberSaveable { mutableStateOf(currentDate.get(Calendar.DAY_OF_MONTH)) }
     val haptic = LocalHapticFeedback.current
 
     KalendarTheme {
@@ -104,35 +53,9 @@ fun Kalendar(
             backgroundColor = calendarBackgroundColor,
         ) {
             KalendarView(
-                currentYear = currentYear,
-                currentMonth = currentMonth,
-                currentMonthNumberOfDays = currentMonthNumberOfDays,
-                todayYear = todayYear,
-                todayMonth = todayMonth,
-                todayDayOfMonth = todayDayOfMonth,
-                selectedYear = selectedYear,
-                selectedMonth = selectedMonth,
-                selectedDayOfMonth = selectedDayOfMonth,
-                startOfMonthDayOfWeek = getDayOfWeek(currentCalendarStartOfMonth.get(Calendar.DAY_OF_WEEK)),
-                onCurrentDayClick = { year: Int, month: Int, day: Int ->
+                onCurrentDayClick = { date ->
                     haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    selectedYear = year
-                    selectedMonth = month
-                    selectedDayOfMonth = day
-                },
-                onPreviousMonthClick = {
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    currentDate.add(Calendar.MONTH, -1)
-                    currentCalendarStartOfMonth.add(Calendar.MONTH, -1)
-                    currentMonth = currentDate.get(Calendar.MONTH)
-                    currentYear = currentDate.get(Calendar.YEAR)
-                },
-                onNextMonthClick = {
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    currentDate.add(Calendar.MONTH, 1)
-                    currentCalendarStartOfMonth.add(Calendar.MONTH, 1)
-                    currentMonth = currentDate.get(Calendar.MONTH)
-                    currentYear = currentDate.get(Calendar.YEAR)
+                    onCurrentDayClick(date)
                 }
             )
         }
