d36f1301ad9d838483512d8fdce7269904370b31
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-01-18 15:26:02 +0100

    Added Internal Modifier

--- kalendar/src/main/java/com/himanshoe/kalendar/ui/oceanic/KalendarOceanHeader.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/ui/oceanic/KalendarOceanHeader.kt
@@ -92,7 +92,7 @@ internal fun KalendarOceanButton(
 }
 
 
-fun LocalDate.getNext7Dates(): List<LocalDate> {
+internal fun LocalDate.getNext7Dates(): List<LocalDate> {
     val dates = mutableListOf<LocalDate>()
     repeat(7) { day ->
         dates.add(this.plusDays(day.toLong()))
@@ -100,7 +100,7 @@ fun LocalDate.getNext7Dates(): List<LocalDate> {
     return dates
 }
 
-fun LocalDate.getPrevious7Dates(): List<LocalDate> {
+internal fun LocalDate.getPrevious7Dates(): List<LocalDate> {
     val dates = mutableListOf<LocalDate>()
     repeat(7) { day ->
         dates.add(this.minusDays(day.toLong()))
