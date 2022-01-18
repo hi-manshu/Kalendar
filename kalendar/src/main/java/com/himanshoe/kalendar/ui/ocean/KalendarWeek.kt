dbcb8e3f6f8b29628af445307387980419032930
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-01-18 09:17:11 +0100

    Added Drag Gesture

--- kalendar/src/main/java/com/himanshoe/kalendar/ui/ocean/KalendarWeek.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/ui/ocean/KalendarWeek.kt
@@ -1,10 +1,14 @@
 package com.himanshoe.kalendar.ui.ocean
 
+import android.util.Log
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import com.himanshoe.design.primitive.texts.Demibold
 import com.himanshoe.design.primitive.texts.KalendarText
@@ -15,13 +19,32 @@ import java.time.LocalDate
 fun KalendarOceanWeek(
     startDate: LocalDate = LocalDate.now(),
 ) {
-    val next7Dates = startDate.getNext7Dates()
-    val previous7Dates = startDate.getPrevious7Dates()
-
-    BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+    val next7Dates = remember {
+        startDate.getNext7Dates()
+    }
+    val previous7Dates = remember {
+        startDate.getPrevious7Dates()
+    }
+    BoxWithConstraints(modifier = Modifier
+        .fillMaxWidth()
+        .pointerInput(Unit) {
+            detectDragGestures { change, dragAmount ->
+                change.consumeAllChanges()
+                val (x, y) = dragAmount
+                when {
+                    x > 0 -> {
+                        Log.d("dsfsfsdfsdf1", "right")
+                    }
+                    x < 0 -> {
+                        Log.d("dsfsfsdfsdf", "left")
+                    }
+                }
+            }
+        }) {
         val size = (maxWidth / 7)
-        LazyRow(modifier = Modifier.fillMaxWidth()) {
-            items(items = next7Dates, itemContent = { item ->
+        val state = rememberLazyListState()
+        Row(modifier = Modifier.fillMaxWidth()) {
+            next7Dates.forEach { item ->
                 Column {
                     KalendarText.Text5.Regular(
                         text = item.dayOfWeek.toString().subSequence(0, 3).toString(),
@@ -34,16 +57,14 @@ fun KalendarOceanWeek(
                         textAlign = TextAlign.Center
                     )
                 }
-
-            })
-
+            }
         }
     }
 }
 
 private fun LocalDate.getNext7Dates(): List<LocalDate> {
     val dates = mutableListOf<LocalDate>()
-    repeat(14) { day ->
+    repeat(7) { day ->
         dates.add(this.plusDays(day.toLong()))
     }
     return dates
