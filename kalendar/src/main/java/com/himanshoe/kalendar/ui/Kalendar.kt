1a7c8f4038714c3294933c8779a6711f0370cff6
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-01-20 14:44:19 +0100

    Update Kalendar.kt

--- kalendar/src/main/java/com/himanshoe/kalendar/ui/Kalendar.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/ui/Kalendar.kt
@@ -2,7 +2,7 @@ package com.himanshoe.kalendar.ui
 /*
 * MIT License
 *
-* Copyright (c) 2022 Shreyas Patil
+* Copyright (c) 2022 Himanshu Singh
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
