381c8aeb4ef7a169cd3c9b22e246198e16e1612f
Gajendra Singh Rathore <niks.gajsa@gmail.com>
2022-03-16 21:32:52 +0530

    unused imports removed

--- app/src/main/java/com/himanshoe/sample/MainActivity.kt
+++ app/src/main/java/com/himanshoe/sample/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.himanshoe.sample
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.himanshoe.kalendar.endlos.common.KalendarKonfig
--- kalendar-endlos/src/main/java/com/himanshoe/kalendar/endlos/ui/KalendarDay.kt
+++ kalendar-endlos/src/main/java/com/himanshoe/kalendar/endlos/ui/KalendarDay.kt
@@ -32,7 +32,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
