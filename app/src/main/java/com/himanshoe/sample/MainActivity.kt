a78b59fa5c2740c88ba2661fddff75b121f1b72a
Gajendra Singh Rathore <niks.gajsa@gmail.com>
2022-03-17 03:16:40 +0530

    Event types added such as - GOAL, REMINDER, TASK, EVENT

--- app/src/main/java/com/himanshoe/sample/MainActivity.kt
+++ app/src/main/java/com/himanshoe/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.himanshoe.kalendar.endlos.common.KalendarKonfig
+import com.himanshoe.kalendar.endlos.common.data.EventType
 import com.himanshoe.kalendar.endlos.common.data.KalendarEvent
 import com.himanshoe.kalendar.endlos.ui.Kalendar
 import java.time.DayOfWeek
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Kalendar(
-                kalendarEvents = listOf(KalendarEvent(LocalDate.now().plusDays(3), "", "")),
+                kalendarEvents = listOf(KalendarEvent(LocalDate.now().plusDays(3), "", "",EventType.GOAL)),
                 kalendarKonfig = KalendarKonfig(weekCharacters = 2, firstDayOfWeek = DayOfWeek.MONDAY),
                 onCurrentDayClick = { date, event ->
                 },
--- kalendar-endlos/src/main/java/com/himanshoe/kalendar/endlos/common/data/KalendarEvent.kt
+++ kalendar-endlos/src/main/java/com/himanshoe/kalendar/endlos/common/data/KalendarEvent.kt
@@ -6,10 +6,19 @@ import java.time.LocalDate
  * [KalendarEvent] handles the event marked on any
  * @param[date] with specific
  * @param[eventName] and its
- * @param[eventDescription]
+ * @param[eventDescription] along with it's
+ * @param[eventType]
  */
 data class KalendarEvent(
     val date: LocalDate,
     val eventName: String,
     val eventDescription: String? = null,
+    val eventType: EventType = EventType.EVENT,
 )
+
+enum class EventType {
+    GOAL,
+    REMINDER,
+    TASK,
+    EVENT
+}
