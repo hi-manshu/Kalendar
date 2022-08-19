3be27c427d6eb4d94e6fdebd2988b432256799c2
Vipul Thawre <rahul12thawre@gmail.com>
2022-08-19 17:06:34 +0530

    * Fixed : ripple and shape for day click

--- kalendar/src/main/java/com/himanshoe/kalendar/component/day/KalendarDay.kt
+++ kalendar/src/main/java/com/himanshoe/kalendar/component/day/KalendarDay.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -56,9 +59,12 @@ fun KalendarDay(
         modifier = modifier
             .border(border = border, shape = CircleShape)
             .clip(shape = shape)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(bounded = true)
+            ) { onCurrentDayClick(kalendarDay, kalendarEvents) }
             .size(size = size)
-            .background(color = bgColor)
-            .clickable { onCurrentDayClick(kalendarDay, kalendarEvents) },
+            .background(color = bgColor),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
@@ -142,7 +148,7 @@ private fun getTextSelectionShape(
 ) = if (kalendarDayState is KalendarDayState.KalendarDaySelected) {
     CircleShape
 } else {
-    RectangleShape
+    CircleShape
 }
 
 private fun getTextColor(
